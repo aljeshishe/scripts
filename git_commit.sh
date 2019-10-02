0a9d74f1be9a4997bb166a62e49e4cb48d03f872
Aleksei.Grachev <aleksei_grachev@epam.com>
2019-10-03 01:07:47 +0300

    Updated scripts

--- git_commit.sh
+++ git_commit.sh
@@ -1,2 +1,3 @@
+chmod -R 777 *.sh
 git commit -m "Updated scripts"
 git push
---
+++ mount_yandex_disk.sh
@@ -0,0 +1,3 @@
+sudo apt install davfs2 -y
+sudo mkdir /mnt/yandex.disk
+sudo mount -t davfs https://webdav.yandex.ru /mnt/yandex.disk/

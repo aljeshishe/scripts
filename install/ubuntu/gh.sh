c2c3bd11554d8c2eda99d28f2a0c338c387f6b8d
Alexey Grachev <ax66@bk.ru>
2021-04-30 19:34:59 +0300

    added gh.sh

---
+++ install/ubuntu/gh.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+set -ex
+
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-key C99B11DEB97541F0
+sudo apt-add-repository https://cli.github.com/packages
+sudo apt update
+sudo apt install gh

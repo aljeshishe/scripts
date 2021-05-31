37e9149e29402ecaf7f0b751799a71cf661fb021
Alexey Grachev <ax66@bk.ru>
2021-05-31 21:45:54 +0300

    change mount_yandex_disk.sh  benchmarks/archives.sh

--- benchmarks/archives.sh
+++ benchmarks/archives.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# set -ex
 FILES=100000
 FILE_SIZE=100k
 
@@ -15,44 +16,43 @@ for (( i = 0; i <= $FILES; i++ ))
 do
 	head -c $FILE_SIZE </dev/urandom > files/file$i
 done
-
+TIME_CMD='/usr/bin/time -f \ttime:%E'
 echo "Creating arcs/files.tar.gz"
-time tar czf  arcs/files.tar.gz files
-echo "Unpacking arcs/files.tar.gz"
-time tar -C out -xzf arcs/files.tar.gz 
-rm -rf out/*
+$TIME_CMD tar czf  arcs/files.tar.gz files
 echo "Unpacking arcs/files.tar.gz 1 file"
-time tar -C out -xf arcs/files.tar.gz files/file50
+$TIME_CMD tar -C out -xf arcs/files.tar.gz files/file50
 rm -rf out/*
-rm -rf arcs
+echo "Unpacking arcs/files.tar.gz"
+$TIME_CMD tar -C out -xzf arcs/files.tar.gz 
+rm -rf out/*
 
+echo
 echo "Creating arcs/files.tar"
-time tar cf arcs/files.tar files
-echo "Unpacking arcs/files.tar"
-time tar -C out -xf arcs/files.tar
-rm -rf out/*
+$TIME_CMD tar cf arcs/files.tar files
 echo "Unpacking arcs/files.tar 1 file"
-time tar -C out -xf arcs/files.tar files/file50
+$TIME_CMD tar -C out -xf arcs/files.tar files/file50
 rm -rf out/*
-rm -rf arcs
+echo "Unpacking arcs/files.tar"
+$TIME_CMD tar -C out -xf arcs/files.tar
+rm -rf out/*
 
 
+echo
 echo "Creating arcs/files.zip"
-time zip -rq -0 arcs/files.zip files
-echo "Unpacking arcs/files.zip"
-time unzip -q arcs/files.zip -d out 
-rm -rf out/*
+$TIME_CMD zip -rq -0 arcs/files.zip files
 echo "Unpacking arcs/files.zip 1 file"
-time unzip -q arcs/files.zip  files/file50 -d out
+$TIME_CMD unzip -q arcs/files.zip  files/file50 -d out
 rm -rf out/*
-rm -rf arcs
+echo "Unpacking arcs/files.zip"
+$TIME_CMD unzip -q arcs/files.zip -d out 
+rm -rf out/*
 
+echo
 echo "Creating arcs/files.7z"
-time 7za a -mx=0 arcs/files.7z files > /dev/null
-echo "Unpacking arcs/files.7z"
-time 7za x -oout arcs/files.7z  > /dev/null
-rm -rf out/*
+$TIME_CMD 7za a -mx=0 arcs/files.7z files > /dev/null
 echo "Unpacking arcs/files.7z 1 file"
-time 7za x -oout arcs/files.7z files/file50 > /dev/null 
+$TIME_CMD 7za x -oout arcs/files.7z files/file50 > /dev/null 
 rm -rf out/*
-rm -rf arcs
+echo "Unpacking arcs/files.7z"
+$TIME_CMD 7za x -oout arcs/files.7z  > /dev/null
+rm -rf out/*
--- mount_yandex_disk.sh
+++ mount_yandex_disk.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -ex
 . install/ubuntu/davfs2.sh
-sudo mkdir /mnt/yandex.disk
+sudo mkdir -p /mnt/yandex.disk
 sudo mount -t davfs https://webdav.yandex.ru /mnt/yandex.disk/

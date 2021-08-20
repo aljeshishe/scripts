fbfd256aa31d7b296dd397e5d668c790389431e0
Alexey Grachev <alexey.grachev@nordigy.ru>
2021-08-20 13:09:46 +0000

    fixed overwrite bashrc for conda installer

--- install/install_miniconda.sh
+++ install/install_miniconda.sh
@@ -4,4 +4,4 @@ set -ex
 wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda.sh
 bash ~/miniconda.sh -b -p ~/miniconda3
 rm ~/miniconda.sh
-echo "export PATH=$PATH:~/miniconda3/bin" > ~/.bashrc
+echo "export PATH=$PATH:~/miniconda3/bin" >> ~/.bashrc

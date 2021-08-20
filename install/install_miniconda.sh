6ee34eb92d3b275f01ade745273d0d5973f036e4
Alexey Grachev <alexey.grachev@nordigy.ru>
2021-08-20 13:14:33 +0000

    added init bash to miniconda installer

--- install/install_miniconda.sh
+++ install/install_miniconda.sh
@@ -5,3 +5,4 @@ wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/
 bash ~/miniconda.sh -b -p ~/miniconda3
 rm ~/miniconda.sh
 echo "export PATH=$PATH:~/miniconda3/bin" >> ~/.bashrc
+~/miniconda3/bin/conda init bash
